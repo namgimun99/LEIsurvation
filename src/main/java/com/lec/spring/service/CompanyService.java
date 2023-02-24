da8cf70fef77faabe4c55b3497eef90988190c95
gimun <ngm011@naver.com>
2023-02-24 20:31:49 +0900

    addauthority

--- src/main/java/com/lec/spring/service/CompanyService.java
+++ src/main/java/com/lec/spring/service/CompanyService.java
@@ -1,8 +1,10 @@
 package com.lec.spring.service;
 
 
+import com.lec.spring.domain.Authority;
 import com.lec.spring.domain.CompanyWrite;
 import com.lec.spring.domain.LeisureWrite;
+import com.lec.spring.repository.AuthorityRepository;
 import com.lec.spring.repository.CompanyWriteRepository;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +17,28 @@ import java.util.List;
 @Service
 public class CompanyService {
     private CompanyWriteRepository companyWriteRepository;
+    private AuthorityRepository authorityRepository;
 
     @Autowired
     public CompanyService(SqlSession sqlSession) {
         companyWriteRepository = sqlSession.getMapper(CompanyWriteRepository.class);
+        authorityRepository = sqlSession.getMapper(AuthorityRepository.class);
     }
 
     public int companyWrite(CompanyWrite companyWrite) {
         return companyWriteRepository.save(companyWrite);
+    }
+
+    public int companyJoin(CompanyWrite companyWrite){
+
+        // 컴퍼니 권한부여
+        Authority auth = authorityRepository.findByName("ROLE_COMPANY");
+
+        String user_id = companyWrite.getUser_id();
+        Long auth_id = auth.getId();
+        authorityRepository.addAuthority(Long.valueOf(user_id), auth_id);
+
+        return 1;
     }
 
     @Transactional
